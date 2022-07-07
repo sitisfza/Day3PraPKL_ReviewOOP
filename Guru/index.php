dc48b60f8a49bcbe759c82dfd537571c302e4103
sitisfza <sitisalfaza1905@gmail.com>
2022-07-07 16:16:41 +0700

    index

--- Guru/index.php
+++ Guru/index.php
@@ -52,7 +52,7 @@
                         <tr>
                             <th scope="col">No</th>
                             <th scope="col">NIP</th>
-                            <th scope="col">Nama Lengkap</th>
+                            <th scope="col">Nama</th>
                             <th scope="col">Alamat</th>
                             <th scope="col">Action</th>
                         </tr>
